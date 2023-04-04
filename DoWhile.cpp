c9ef87b3376977a145e3abaeaf385e410ea6800b
CyanStar01 <128439233+CyanStar01@users.noreply.github.com>
2023-04-03 21:53:27 -0300

    Update DoWhile.cpp

--- DoWhile.cpp
+++ DoWhile.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int num;
+int main() { // chave principal onde nosso codigo vai ser escrito
+    int num; // cria a variavel int que iremos usar
     
-    do {
+    do { // codigo dentro de "do" eh executado desde que a condicao de "while" seja verdadeira
         cout << "Digite um numero entre 1 e 10: ";
         cin >> num;
-    } while(num < 1 || num > 10);
+    } while(num < 1 || num > 10); // atribui uma condicao para a funcao "do" ser executada
     
-    cout << "Você digitou o numero " << num << ".";
+    cout << "Você digitou o numero " << num << "."; // mostra o numero digitado pelo usuario
 
     return 0;
 }
